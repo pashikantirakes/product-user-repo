c81bcb27c1855b5b37c4f042a2393bab9b68fc53
pashikantirakes <rakeshpashikanti740@gmail.com>
2024-12-18 17:23:02 +0530

    removed order service

--- OrderService/src/main/java/com/example/OrderService/feign/WebClientEx.java
+++
@@ -1,9 +0,0 @@
-package com.example.OrderService.feign;
-
-import org.springframework.web.reactive.function.client.WebClient;
-
-public class WebClientEx {
-    public WebClient.Builder builder(){
-        return (WebClient.Builder) WebClient.builder().build();
-    }
-}
